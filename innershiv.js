334d2fbaba87ee25405da378a079a9110a21c2e7
thegrandpoobah <sahab.yazdani+github@gmail.com>
2010-07-06 15:32:40 -0400

    add the ability to interpret xhtml style markup.

--- innershiv.js
+++ innershiv.js
@@ -1,6 +1,14 @@
 // http://jdbartlett.github.com/innershiv
 window.innerShiv = (function() {
 	var d, r;
+	// from jQuery, add the ability to interpret xhtml style tags
+    var rxhtmlTag = /(<([\w:]+)[^>]*?)\/>/g;
+    var rselfClosing = /^(?:area|br|col|embed|hr|img|input|link|meta|param)$/i;
+    var fcloseTag = function(all, front, tag) {
+        return rselfClosing.test(tag) ?
+			all :
+			front + "></" + tag + ">";
+    };
 	
 	return function(h, u) {
 		if (!d) {
@@ -11,6 +19,8 @@ window.innerShiv = (function() {
 		
 		var e = d.cloneNode(true);
 		/*@cc_on document.body.appendChild(e);@*/
+		// from jQuery, add the ability to interpret xhtml style tags
+		h = h.replace(rxhtmlTag, fcloseTag);
 		e.innerHTML = h;
 		/*@cc_on document.body.removeChild(e);@*/
 		
