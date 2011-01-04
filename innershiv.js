d27321b8d96fe5f8c789733949b302a28f2e71c3
thegrandpoobah <sahab.yazdani+github@gmail.com>
2011-01-04 13:50:11 -0500

    Fixes issue 1 (table elements don't work with innershiv)

--- innershiv.js
+++ innershiv.js
@@ -1,8 +1,8 @@
 // http://jdbartlett.github.com/innershiv | WTFPL License
 window.innerShiv = (function() {
 	var d, r;
-	// from jQuery, add the ability to interpret xhtml style tags
-    var rxhtmlTag = /(<([\w:]+)[^>]*?)\/>/g;
+    var rxhtmlTag = /(<([\w:]+)[^>]*?)\/>/g; // from jQuery, add the ability to interpret xhtml style tags
+	var tableFix = /^<(tbody|tr|td|col|colgroup|thead|tfoot)/i;
     var rselfClosing = /^(?:area|br|col|embed|hr|img|input|link|meta|param)$/i;
     var fcloseTag = function(all, front, tag) {
         return rselfClosing.test(tag) ?
@@ -17,12 +17,22 @@ window.innerShiv = (function() {
 			/*@cc_on d.style.display = 'none';@*/
 		}
 		
+		var matches = h.match(tableFix), tagName;
+		if (matches) {
+			tagName = matches[1];
+			h = '<table>' + h + '</table>';
+		}
+		
 		var e = d.cloneNode(true);
 		/*@cc_on document.body.appendChild(e);@*/
 		e.innerHTML = h.replace(rxhtmlTag, fcloseTag) // from jQuery, add the ability to interpret xhtml style tags
 			.replace(/^\s\s*/, '')
 			.replace(/\s\s*$/, '');
 		/*@cc_on document.body.removeChild(e);@*/
+		
+		if (matches) {
+			e = e.getElementsByTagName(tagName)[0].parentNode;
+		}
 		
 		if (u === false) return e.childNodes;
 		
