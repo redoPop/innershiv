47f27e3c4bafe2f9890c6d830031d52c45bbe9b0
joe bartlett <contact@jdbartlett.com>
2011-02-13 21:36:10 -0600

    Cleanup after pull request.

--- innershiv.js
+++ innershiv.js
@@ -1,43 +1,36 @@
 // http://jdbartlett.github.com/innershiv | WTFPL License
 window.innerShiv = (function() {
-	var d, r;
-    var rxhtmlTag = /(<([\w:]+)[^>]*?)\/>/g; // from jQuery, add the ability to interpret xhtml style tags
-	var tableFix = /^<(tbody|tr|td|col|colgroup|thead|tfoot)/i;
-    var rselfClosing = /^(?:area|br|col|embed|hr|img|input|link|meta|param)$/i;
-    var fcloseTag = function(all, front, tag) {
-        return rselfClosing.test(tag) ?
-			all :
-			front + "></" + tag + ">";
-    };
-	
-	return function(h, u) {
-		if (!d) {
-			d = document.createElement('div');
-			r = document.createDocumentFragment();
-			/*@cc_on d.style.display = 'none';@*/
+	var div, frag,
+		inaTable = /^<(tbody|tr|td|col|colgroup|thead|tfoot)/i,
+		remptyTag = /(<([\w:]+)[^>]*?)\/>/g,
+		rselfClosing = /^(?:area|br|col|embed|hr|img|input|link|meta|param)$/i,
+		fcloseTag = function (all, front, tag) {
+			return rselfClosing.test(tag) ? all : front + '></' + tag + '>';
+		};
+
+	return function(html, returnFrag) {
+		if (!div) {
+			div = document.createElement('div');
+			frag = document.createDocumentFragment();
+			/*@cc_on div.style.display = 'none';@*/
 		}
-		
-		h = h.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-		
-		var matches = h.match(tableFix);
-		if (matches) {
-			h = '<table>' + h + '</table>';
-		}
-		
-		var e = d.cloneNode(true);
-		/*@cc_on document.body.appendChild(e);@*/
-		e.innerHTML = h.replace(rxhtmlTag, fcloseTag); // from jQuery, add the ability to interpret xhtml style tags
-		/*@cc_on document.body.removeChild(e);@*/
-		
-		if (matches) {
-			e = e.getElementsByTagName(matches[1])[0].parentNode;
-		}
-		
-		if (u === false) return e.childNodes;
-		
-		var f = r.cloneNode(true), i = e.childNodes.length;
-		while (i--) f.appendChild(e.firstChild);
-		
-		return f;
+
+		html = html.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+
+		var tabled = html.match(inaTable), myDiv = div.cloneNode(true);
+		if (tabled) html = '<table>' + html + '</table>';
+
+		/*@cc_on document.body.appendChild(myDiv);@*/
+		myDiv.innerHTML = html.replace(remptyTag, fcloseTag);
+		/*@cc_on document.body.removeChild(myDiv);@*/
+
+		if (tabled) myDiv = myDiv.getElementsByTagName(tabled[1])[0].parentNode;
+
+		if (returnFrag === false) return myDiv.childNodes;
+
+		var myFrag = frag.cloneNode(true), i = myDiv.childNodes.length;
+		while (i--) myFrag.appendChild(myDiv.firstChild);
+
+		return myFrag;
 	}
 }());
