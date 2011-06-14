be15b1c450d41786c1ebe176f4de09336094b3c9
joe bartlett <contact@jdbartlett.com>
2011-06-14 08:50:58 -0500

    Added verbose documentation and license file.

--- innershiv.src.js
+++ innershiv.src.js
@@ -1,11 +1,19 @@
-// http://jdbartlett.github.com/innershiv | WTFPL License
+/* innerShiv: makes HTML5shim work on innerHTML & jQuery
+ * http://jdbartlett.github.com/innershiv
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
 window.innerShiv = (function () {
 	var div;
 	var doc = document;
 	var needsShiv;
 	
 	// Array of elements that are new in HTML5
-	var html5 = 'abbr article aside audio canvas command datalist details figure figcaption footer header hgroup keygen mark meter nav output progress section source summary time video'.split(' ');
+	var html5 = 'abbr article aside audio canvas datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.split(' ');
 	
 	// Used to idiot-proof self-closing tags
 	function fcloseTag(all, front, tag) {
